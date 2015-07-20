19a830c3ef39978e7bc5092fdc3567c3dcb58a15
chriskjaer <chriskjaer.dk@gmail.com>
2015-07-21 00:08:10 +0200

    [config] white space and backup/autosave rules

--- init.el
+++ init.el
@@ -1,5 +1,3 @@
-(global-linum-mode t)
-
 (require 'package)
 
 (setq package-enable-at-startup nil)
@@ -18,17 +16,44 @@
 
 
 
-;;; GUI
+;;; GUI Niceness
+(menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
+(column-number-mode -1)
+(line-number-mode t)
 
+;; 80 Char column lines
+(setq-default fill-column 80
+              whitespace-line-column 80)
 
+;; Two space. No tabs
+(setq-default indent-tabs-mode nil
+              tab-width 2)
+
+;; Remove trailing whitespace on save
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+;; Whitespace rules
+(setq whitespace-style '(indentation::space
+                         space-after-tab
+                         space-before-tab
+                         trailing
+                         lines-tail
+                         tab-mark
+                         face
+                         tabs))
 
 ;;; Misc
 (setq make-backup-files nil)
 (setq ring-bell-function 'ignore)
+(setq inhibit-splash-screen t) ;; remove splash screen
 
-
+;; Move autosave and backup files to temp dir.
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
 
 ;;; Theme
 (load-theme 'wombat)
@@ -40,9 +65,7 @@
 (ido-mode 1)
 (ido-everywhere 1)
 (flx-ido-mode 1)
-;;; disable ido faces to see flx highlights.
-(setq ido-enable-flex-matching t)
+(setq ido-enable-flex-matching t) ;; disable ido faces to see flx highlights.
 (setq ido-use-faces nil)
-
 (setq ido-decorations
       (quote ("\n↪ "     "" "\n   " "\n   ..." "[" "]" " [No match]" " [Matched]" " [Not readable]" " [Too big]" " [Confirm]")))
